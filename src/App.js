f4f986de48e01440860f03116542b26692b1ffa7
Jarrod Goodney <jarrod.goodney@gmail.com>
2021-02-24 20:18:55 -0500

    added logic responsible for actually storing and displaying expression results

--- src/App.js
+++ src/App.js
@@ -9,29 +9,75 @@ import Keypad from './components/keypad.js';
 function App() {
 
   const [input, setInput] = useState([]);
-  const [storedInput, setStoredInput] = useState(0);
+  const [storedInput, setStoredInput] = useState([]);
+  const [result, setResult] = useState(undefined); 
   const [counter, setCounter] = useState(0);
   const [validZeroChecker, setValidZeroChecker] = useState(0);
   const [validDecimalChecker, setValidDecimalChecker] = useState(0);
-  const [operatorInput, setOperatorInput] = useState('null');
+  const [operatorInput, setOperatorInput] = useState(undefined);
+
+
+  const clearHooks = (value) => {
+    setStoredInput([]);
+    setInput([]);
+    setResult(value);
+  }
 
 
   const executeOperation = () => {
-    let itemOne = storedInput;
-
-
+    let itemOne = parseFloat(storedInput);
     let itemTwo = parseFloat(input.join(''));
 
-    if(operatorInput === 'add') {
-      let sum = itemOne+itemTwo;
-      console.log(`${itemOne} + ${itemTwo} = ${sum}`)
-      setStoredInput(storedInput - storedInput);
-      setInput([]);
-      console.log(storedInput);
+    if(result === undefined) {
+      switch(operatorInput) {
+        case('+'):
+          let sum = itemOne + itemTwo;
+          console.log(`${itemOne} ${operatorInput} ${itemTwo} = ${sum}`);
+          clearHooks(sum);
+          break;
+        case('-'):
+          let difference = itemOne - itemTwo;
+          console.log(`${itemOne} ${operatorInput} ${itemTwo} = ${difference}`);
+          clearHooks(difference);
+          break;
+        case('x'):
+          let product = itemOne * itemTwo;
+          console.log(`${itemOne} ${operatorInput} ${itemTwo} = ${product}`);
+          clearHooks(product);
+          break;
+        case('/'):
+          let quotient = itemOne / itemTwo;
+          console.log(`${itemOne} ${operatorInput} ${itemTwo} = ${quotient}`);
+          clearHooks(quotient);
+          break;      
+      }
     } else {
-      console.log('wack');
+      switch(operatorInput) {
+        case('+'):
+          let sum = result + itemTwo;
+          console.log(`${result} ${operatorInput} ${itemTwo} = ${sum}`);
+          clearHooks(sum);
+          break;
+        case('-'):
+          let difference = result - itemTwo;
+          console.log(`${result} ${operatorInput} ${itemTwo} = ${difference}`);
+          clearHooks(difference);
+          break;
+        case('x'):
+          let product = result * itemTwo;
+          console.log(`${result} ${operatorInput} ${itemTwo} = ${product}`);
+          clearHooks(product);
+          break;
+        case('/'):
+          let quotient = result / itemTwo;
+          console.log(`${result} ${operatorInput} ${itemTwo} = ${quotient}`);
+          clearHooks(quotient);
+          break;      
+      }
     }
   }
+
+
 
   const handleClick = (e) => {
     if(e.target.id === "clear") {
@@ -40,27 +86,29 @@ function App() {
       setInput([]);
       setCounter(counter => counter + 1);
       setValidDecimalChecker(0);
-      if(counter === 2) {
+      if(counter === 1) {
         setCounter(0);
         setInput([]);
-        setStoredInput(0);
+        setStoredInput([]);
+        setResult(undefined)
       }
 
       //Checks input for non-numeral input, stores current input into StoredInput array of arrays
     } else if(e.target.id === "add" || e.target.id === "multiply" || e.target.id === "divide" || e.target.id === "subtract" || e.target.id === "add" || e.target.id === "equals" || e.target.id === "+/-" ) {
-      setStoredInput(parseFloat(input.join('')));
+      console.log(parseFloat(input.join('')))
+      setStoredInput([...storedInput, parseFloat(input.join(''))]);
       setInput([]);
       setValidDecimalChecker(0);
 
       if(e.target.id === 'add') {
-        setOperatorInput(operator => operator = 'add');
+        setOperatorInput(operator => operator = '+');
       } else if(e.target.id === 'subtract') {
-        setOperatorInput(operator => operator = 'subtract');
+        setOperatorInput(operator => operator = '-');
       } else if(e.target.id === 'multiply') {
-        setOperatorInput(operator => operator = 'multiply');
+        setOperatorInput(operator => operator = 'x');
       } else if(e.target.id === 'divide') {
-        setOperatorInput(operator => operator = 'divide');
-      } else if(e.target.id === 'equals' && operatorInput !== 'null') {
+        setOperatorInput(operator => operator = '/');
+      } else if(e.target.id === 'equals' && operatorInput !== undefined) {
         executeOperation()
       }
 
@@ -78,12 +126,9 @@ function App() {
 
   }
 
-
-
-
   return (
     <div className="App">
-      <Output input={input}/>
+      <Output input={input} stored={storedInput} result={result} operator={operatorInput}/>
       <Keypad handleClick={handleClick}/>
     </div>
   );
--- src/components/output.js
+++ src/components/output.js
@@ -2,13 +2,32 @@ import React from 'react'
 
 //Component renders outputs
 export default function output(props) {
-    return (
+    if(props.result === undefined) {
+        return (
+            <div id="output-container">
+                <div id="display">
+                    {props.input.length ===  0 ? '0' : props.input.map(item => {
+                        return(item)
+                    })}
+
+                </div>
+            </div>
+        )
+    } else if(props.result !== undefined) {
+        return (
         <div id="output-container">
             <div id="display">
-                {props.input.length ===  0 ? '0' : props.input.map(item => {
-                    return(item)
-                })}
+                {props.result}
             </div>
         </div>
-    )
+        )
+    } else if(props.result !== undefined && props.operatorInput !== 'null') {
+        return (
+        <div id="output-container">
+            <div id="display">
+                {`${props.result} ${props.operator} ${props.input}`}
+            </div>
+        </div>
+        )
+    }
 }
